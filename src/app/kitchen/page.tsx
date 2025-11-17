119f1f910933813dcbf5332fc10b5b86368a1dd9
atukenov <almaz.t97@gmail.com>
2025-11-18 01:45:01 +0500

    init

---
+++ src/app/kitchen/page.tsx
@@ -0,0 +1,16 @@
+import { Lottie } from 'xtreme-ui';
+
+import { getAnimSrc } from '#utils/constants/common';
+
+import './kitchen.scss';
+
+const Kitchen = () => {
+	return (
+		<div className='kitchen'>
+			<Lottie src={getAnimSrc('UnderConstruction')} size={450} />
+			<p>This page is under development</p>
+		</div>
+	);
+};
+
+export default Kitchen;
